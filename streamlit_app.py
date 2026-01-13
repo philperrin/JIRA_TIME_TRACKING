728d6f630a0d9d1fa5e0bdff0f4c18a264a4210e
philperrin <35937414+philperrin@users.noreply.github.com>
2026-01-12 17:32:36 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
-import streamlit as st
+pages = {
+    "Your account": [
+        st.Page("main.py", title="Log time to Jira"),
+        st.Page("config.py", title="Configure"),
+        st.Page("reports.py", title="Reports"),
+    ],
+}
 
-def config():
-    st.title("Config")
-def reports():
-    st.title("Reports")
-
-pg = st.navigation(["main.py", config, reports])
+pg = st.navigation(pages, position="top")
 pg.run()
