2caca080d688410342e1a809737227b7436ce80e
philperrin <35937414+philperrin@users.noreply.github.com>
2026-01-12 17:20:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 def page_2():
     st.title("Config")
 def page_3():
-    st.title("Page 3")
+    st.title("Reports")
 
-pg = st.navigation(["main.py", config, page_3])
+pg = st.navigation(["main.py", config, reports])
 pg.run()
