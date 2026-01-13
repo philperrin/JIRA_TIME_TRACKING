274f4934f49354786b263af5044f1cda45dae82a
philperrin <35937414+philperrin@users.noreply.github.com>
2026-01-12 20:45:54 -0700

    Update log.py

--- log.py
+++ log.py
@@ -1,3 +1,11 @@
 import streamlit as st
-
 st.title("Log time to Jira")
+
+st.text("Use this page to assign your time to specific tasks in Jira.")
+
+st.subheader("Components to build:", divider="gray")
+st.markdown(":pencil2:   Quick summary - hrs in Jira for day and week")
+st.markdown(":pencil2:   How to best populate time to task.... log line to populate to df w/ commit button?")
+st.markdown(":pencil2:   Required: date, task (select from jira list), start datetime, duration (or end time)")
+st.markdown(":pencil2:   Optional: comments")
+st.markdown(":pencil2:   Commit button")
