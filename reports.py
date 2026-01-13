6bdcb16951af8d8a3d8118cff0dd8d855c032066
philperrin <35937414+philperrin@users.noreply.github.com>
2026-01-12 21:30:39 -0700

    Update reports.py

--- reports.py
+++ reports.py
@@ -4,7 +4,7 @@ st.title("Reports")
 
 st.text("Use this page show user insights.")
 
-st.subheader("Components to build:", divider="gray")
-st.markdown(":pencil2:   Current week: hours to allcations")
-st.markdown(":pencil2:   Current month: hours to allocations")
-st.markdown(":pencil2:   Current year: hours to allocations")
+with st.expander("Components to build:"):
+  st.markdown(":pencil2:   Current week: hours to allcations")
+  st.markdown(":pencil2:   Current month: hours to allocations")
+  st.markdown(":pencil2:   Current year: hours to allocations")
