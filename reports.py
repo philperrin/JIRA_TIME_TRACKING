dc7935069df7132f5c06ab5de3bcbc2d98a03f15
philperrin <35937414+philperrin@users.noreply.github.com>
2026-01-12 21:16:41 -0700

    Update reports.py

--- reports.py
+++ reports.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.title("Reports")
+
+st.text("Use this page show user insights.")
+
+st.subheader("Components to build:", divider="gray")
+st.markdown(":pencil2:   Current week: hours to allcations")
+st.markdown(":pencil2:   Current month: hours to allocations")
+st.markdown(":pencil2:   Current year: hours to allocations")
