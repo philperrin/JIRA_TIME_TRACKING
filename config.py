55a77e23bebf4ea0c57d2e1e25d4394ae79f5eb5
philperrin <35937414+philperrin@users.noreply.github.com>
2026-01-12 20:38:02 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
 import streamlit as st
-
 st.title("Config")
+
+st.text("Use this page to provide configuration details necessary for the app.")
+
+st.subheader("Components to build:", divider="gray")
+st.markdown(":pencil2:   Input user email")
+st.markdown(":pencil2:   Input Jira filter id")
+st.markdown(":pencil2:   Input Jira API key")
+st.markdown(":pencil2:   Collect user Jira id (requires user email and API key)")
+st.markdown(":pencil2:   Input allocations (Jira project id, client name, project name, weekly hrs, effective dates)")
