ca64a5c965c4b82bd64f931e8d8d64c72cc5fa89
philperrin <35937414+philperrin@users.noreply.github.com>
2026-01-12 21:28:39 -0700

    Update log.py

--- log.py
+++ log.py
@@ -3,16 +3,16 @@ st.title("Log time to Jira")
 
 st.text("Use this page to assign your time to specific tasks in Jira.")
 
-st.subheader("Components to build:", divider="gray")
-st.markdown(":pencil2:   Quick summary - hrs in Jira for day and week")
-st.markdown(":pencil2:   How to best populate time to task.... log line to populate to df w/ commit button?")
-st.markdown(":pencil2:   Required: date, task, start datetime, duration (or end time)")
-st.markdown(":pencil2:   Optional: comments")
-st.markdown(":pencil2:   Commit button")
+with st.expander("Components to build:"):
+  st.markdown(":pencil2:   Quick summary - hrs in Jira for day and week")
+  st.markdown(":pencil2:   How to best populate time to task.... log line to populate to df w/ commit button?")
+  st.markdown(":pencil2:   Required: date, task, start datetime, duration (or end time)")
+  st.markdown(":pencil2:   Optional: comments")
+  st.markdown(":pencil2:   Commit button")
 
-st.subheader("Functionalities:", divider="gray")
-st.markdown(":firecracker:   Time log input device")
-st.markdown(":boom:   Only show if config details present")
-st.markdown(":boom:   Show input elements: date (cal input), task (dropdown from allocations), start (time), end (time), capture button")
-st.markdown(":boom:   Capture dataframe: each time capture button is clicked, add log details to dataframe")
-st.markdown(":boom:   Submit dataframe: send each log details to Jira, store log details to table (with extra details)")
+with st.expander("Functionalities:"):
+  st.markdown(":firecracker:   Time log input device")
+  st.markdown(":boom:   Only show if config details present")
+  st.markdown(":boom:   Show input elements: date (cal input), task (dropdown from allocations), start (time), end (time), capture button")
+  st.markdown(":boom:   Capture dataframe: each time capture button is clicked, add log details to dataframe")
+  st.markdown(":boom:   Submit dataframe: send each log details to Jira, store log details to table (with extra details)")
