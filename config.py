83e28ed7fead199fcf43ea79617d847a8762b075
philperrin <35937414+philperrin@users.noreply.github.com>
2026-01-16 20:43:27 -0700

    Comment out Jira secret creation and request handling
    Comment out the creation and update of Jira authentication secrets and related request handling.

--- config.py
+++ config.py
@@ -52,10 +52,10 @@ def config_modal():
           ALLOWED_AUTHENTICATION_SECRET = '{jira_cred_name}'
           ENABLED = TRUE;
       """
-      #create_int = f"CREATE OR REPLACE SECRET {jira_cred_name} TYPE = GENERIC_STRING SECRET_STRING = {"email": "user_email_input, "api_token": '{api_key}'};"
+      ###create_int = f"CREATE OR REPLACE SECRET {jira_cred_name} TYPE = GENERIC_STRING SECRET_STRING = {"email": "user_email_input, "api_token": '{api_key}'};"
       active_session.sql(create_secret_sql).collect()
-      #update_auth_sec = f"ALTER EXTERNAL ACCESS INTEGRATION jira_access_integration ALLOWED_AUTHENTICATION_SECRET = ({jira_cred_name}) ENABLED = TRUE;"
-      active_session.sql(update_auth_sec).collect()
+      ###update_auth_sec = f"ALTER EXTERNAL ACCESS INTEGRATION jira_access_integration ALLOWED_AUTHENTICATION_SECRET = ({jira_cred_name}) ENABLED = TRUE;"
+      #active_session.sql(update_auth_sec).collect()
       st.session_state["submission_data"] = {"email": user_email_input, "filter_id": filter_id, "api_key": api_key}
       updated_at = datetime.now()
       url = f"https://phdata.atlassian.net/rest/api/3/user/search?query={user_email}"
@@ -63,15 +63,15 @@ def config_modal():
       headers = {
           "Accept": "application/json"
       }
-      try:
-          response = requests.get(
-              url,
-              headers=headers,
-              auth=auth
-          )
-      except requests.exceptions.RequestException as e:
-          st.error(f"Error connecting to Jira: {e}")
-          st.error(f"Response: {response.text if 'response' in locals() else 'No response'}")
+      #try:
+          #response = requests.get(
+          #    url,
+          #    headers=headers,
+          #    auth=auth
+          #)
+      #except requests.exceptions.RequestException as e:
+          #st.error(f"Error connecting to Jira: {e}")
+          #st.error(f"Response: {response.text if 'response' in locals() else 'No response'}")
 
       if user_email_input and filter_id and api_key:
           try:
