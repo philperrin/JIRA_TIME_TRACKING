8c65fc409b3d5a4a6585f38de60f01a077167c27
philperrin <35937414+philperrin@users.noreply.github.com>
2026-01-17 10:58:50 -0700

    Update config.py

--- config.py
+++ config.py
@@ -10,14 +10,13 @@ import json
 
 TABLE_NAME = "CONFIG_DETAILS"
 COL1 = "USER_EMAIL" 
-#COL2 = "FILTER_ID"
-COL3 = "API_KEY" 
-COL4 = "UPDATED_AT"
-COL5 = "JIRA_USER_ID"
+COL2 = "API_KEY" 
+COL3 = "UPDATED_AT"
+COL4 = "JIRA_USER_ID"
 
 
-st.title("Config")
-st.text("Use this page to provide configuration details necessary for the app.")
+st.title("Time Tracking Configuration")
+st.text("Use this page to provide your Jira API key and update your weekly project allocations.")
 
 active_session = get_active_session()
 
@@ -25,19 +24,12 @@ if 'df' not in st.session_state:
     st.session_state.df = pd.DataFrame(columns=['USER_EMAIL','FILTER_ID','API_KEY','UPDATED_AT'])
 
 #Config Modal
-@st.dialog("Jira Configuration Details")
+@st.dialog("Add API Key")
 def config_modal():
-  #if st.user and "email" in st.user:
-    #user_email = st.user["email"]
-  #else:
-    #user_email = "EMAIL REQUIRED"
   with st.form("config_form", clear_on_submit=True):
-    #user_email_input = st.text_input("Email", value=user_email)
     user_email = st.user["email"]
-    #username = user_email_input.split('@')[0]
     username = user_email.split('@')[0]
     jira_cred_name = "jira_credentials_"+username
-    #filter_id = st.text_input("Jira filter id")
     api_key = st.text_input("Jira API key")
     submitted = st.form_submit_button("Submit Details")
     secret_string = "'{ \"email\": \"" + user_email + "\" , \"api_token\": \"" + api_key + "\" }';"
@@ -68,11 +60,9 @@ def config_modal():
       """
       active_session.sql(update_auth_sec)
       
-      #st.session_state["submission_data"] = {"email": user_email_input, "filter_id": filter_id, "api_key": api_key}
       st.session_state["submission_data"] = {"email": user_email, "api_key": api_key}
       updated_at = datetime.now()
       
-      #auth = HTTPBasicAuth(user_email_input, api_key)
       auth = HTTPBasicAuth(user_email, api_key)
       headers = {
           "Accept": "application/json"
@@ -87,13 +77,11 @@ def config_modal():
           st.error(f"Error connecting to Jira: {e}")
           st.error(f"Response: {response.text if 'response' in locals() else 'No response'}")
 
-      #if user_email_input and filter_id and api_key:
       if user_email and api_key:
           try:
               jira_user_id_r = json.loads(response.text)
               jira_user_id = jira_user_id_r[0]['accountId']
-              #insert_query = f"INSERT INTO {TABLE_NAME} ({COL1},{COL2},{COL3},{COL4},{COL5}) VALUES (UPPER('{user_email_input}'),'{filter_id}','{api_key}','{updated_at}','{jira_user_id}')"
-              insert_query = f"INSERT INTO {TABLE_NAME} ({COL1},{COL3},{COL4},{COL5}) VALUES (UPPER('{user_email}'),'{api_key}','{updated_at}','{jira_user_id}')"
+              insert_query = f"INSERT INTO {TABLE_NAME} ({COL1},{COL2},{COL3},{COL4}) VALUES (UPPER('{user_email}'),'{api_key}','{updated_at}','{jira_user_id}')"
               active_session.sql(insert_query).collect()
               st.success("Config details saved!")
           except Exception as e:
@@ -117,10 +105,10 @@ def allocation_modal():
 #Standard Page
 col1, col2 = st.columns(2)
 with col1:
-  if st.button("Open config form", use_container_width=True):
+  if st.button("Add API Key", use_container_width=True):
     config_modal()
 with col2:
-  if st.button("Open allocation form", use_container_width=True):
+  if st.button("Add Allocations", use_container_width=True):
     allocation_modal()
 
 with st.expander("Components to build:"):
@@ -129,17 +117,15 @@ with st.expander("Components to build:"):
   st.markdown(":white_check_mark:   Input Jira API key")
   st.markdown(":white_check_mark:   Mask Jira API key")
   st.markdown(":white_check_mark:   Create view showing user's most recent submission")
-  st.markdown(":pencil2:   Collect user Jira id (requires user email and API key)")
+  st.markdown(":white_check_mark:   Collect user Jira id (requires user email and API key)")
+  st.markdown(":pencil2:   Build a paginated display of issues")
   st.markdown(":pencil2:   Input allocations")
-  st.markdown(":pencil2:   Collect issues from Jira filter id")
-  st.markdown(":pencil2:   Create row access policies based on CURRENT_USER in tables in env schema")
-  st.markdown(":pencil2:   If filter id exists, show current filter logic")
+  st.markdown(":pencil2:   For issues: use (assigned or watching) and != Done")
 
 with st.expander("Functionalities:"):
   st.markdown(":firecracker:   Button to pop up base config modal")
-  st.markdown(":white_check_mark:   Modal to collect user email, filter id, API key")
-  st.markdown(":white_check_mark:   On modal submit -> store details in config table")
-  st.markdown(":boom:   On modal submit -> collect user Jira id and store with config details")
+  st.markdown(":white_check_mark:   Modal to collect API key")
+  st.markdown(":white_check_mark:   On modal submit -> collect API key, user Jira id and store with config details")
   st.markdown(":firecracker:   Button to pop up allocation modal")
   st.markdown("  :boom:   Modal has input table for: Jira project id, client name, project name, weekly hrs, effective dates")
   st.markdown("  :boom:   If allocation table has data in it, display current allocation details - including link to Jira board")
