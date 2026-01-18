70cf815b444829f0a43bcf997fb6b788d4a262ee
philperrin <35937414+philperrin@users.noreply.github.com>
2026-01-18 15:28:21 -0700

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ COL4 = "JIRA_USER_ID"
 
 
 st.title("🛠 Time Tracking Configuration")
-st.text("Use this page to provide your Jira API key and update your weekly project allocations.")
+st.text("Use this page to provide your Jira API token and update your weekly project allocations.")
 
 active_session = get_active_session()
 
@@ -24,13 +24,14 @@ if 'df' not in st.session_state:
     st.session_state.df = pd.DataFrame(columns=['USER_EMAIL','FILTER_ID','API_KEY','UPDATED_AT'])
 
 #Config Modal
-@st.dialog("Add API Key")
+@st.dialog("Add API Token")
 def config_modal():
   with st.form("config_form", clear_on_submit=True):
     user_email = st.user["email"]
     username = user_email.split('@')[0]
     jira_cred_name = "jira_credentials_"+username
-    api_key = st.text_input("Jira API key")
+    st.markdown(f"Click [here](https://id.atlassian.com/manage-profile/security/api-tokens) to generate a Jira API token.")
+    api_key = st.text_input("Jira API token")
     submitted = st.form_submit_button("Submit Details")
     secret_string = "'{ \"email\": \"" + user_email + "\" , \"api_token\": \"" + api_key + "\" }';"
     url = f"https://phdata.atlassian.net/rest/api/3/user/search?query={user_email}"
@@ -83,7 +84,7 @@ def config_modal():
               jira_user_id = jira_user_id_r[0]['accountId']
               insert_query = f"INSERT INTO {TABLE_NAME} ({COL1},{COL2},{COL3},{COL4}) VALUES (UPPER('{user_email}'),'{api_key}','{updated_at}','{jira_user_id}')"
               active_session.sql(insert_query).collect()
-              st.success("API key securely stored and configuration complete!")
+              st.success("API token securely stored and configuration complete!")
           except Exception as e:
               st.error(f"Error: {e}")
       else:
@@ -105,7 +106,7 @@ def allocation_modal():
 #Standard Page
 col1, col2 = st.columns(2)
 with col1:
-  if st.button("Add API Key", use_container_width=True):
+  if st.button("Add API Token", use_container_width=True):
     config_modal()
 with col2:
   if st.button("Add Allocations", use_container_width=True):
@@ -115,57 +116,81 @@ with col2:
 #If there is no api_key then none of this happens.
 #If there is an api_key then try:
 
+
+
 user_email = st.user["email"].upper()
 api_query = f"""
 SELECT API_KEY FROM JIRA_TIME_TRACKING.TEST.CONFIG_DETAILS_LATEST WHERE USER_EMAIL = \'{user_email}\';
 """
 active_session.sql(api_query).collect()
-api_query_res = active_session.sql(api_query)
-api_query_res_arr = api_query_res.collect()[0]['API_KEY']
-GET_ISSUES = "https://phdata.atlassian.net/rest/api/3/search/jql"
-auth = HTTPBasicAuth(user_email, api_query_res_arr)
-headers = {
-    "Accept": "application/json"
-}
-params = {
-    "jql": '(assignee = currentUser() OR watcher = currentUser()) AND status != Done ORDER BY key ASC',
-    "fields": 'key, summary, status, created, customfield_10201, project',
-    "maxResults": 200,
-    "startAt": 0,
-    "expand": "string"
-}
+api_query_count = f"""
+SELECT COUNT(*) AS row_count FROM JIRA_TIME_TRACKING.TEST.CONFIG_DETAILS_LATEST WHERE USER_EMAIL = \'{user_email}\';
+"""
+
 try:
-    response2 = requests.get(
-        GET_ISSUES,
-        headers=headers,
-        params=params,
-        auth=auth
-    )
-except requests.exceptions.RequestException as e:
-    st.error(f"Error connecting to Jira: {e}")
-    st.error(f"Response: {response2.text if 'response' in locals() else 'No response'}")
-issue_result = json.loads(response2.text)
+    api_count = active_session.sql(api_query_count).to_pandas()
+    if not api_count.empty:
+        api_query_res = active_session.sql(api_query)
+        api_query_res_arr = api_query_res.collect()[0]['API_KEY']
+        GET_ISSUES = "https://phdata.atlassian.net/rest/api/3/search/jql"
+        auth = HTTPBasicAuth(user_email, api_query_res_arr)
+        headers = {
+            "Accept": "application/json"
+        }
+        params = {
+            "jql": '(assignee = currentUser() OR watcher = currentUser()) AND status != Done ORDER BY key ASC',
+            "fields": 'key, summary, status, created, customfield_10201, project',
+            "maxResults": 200,
+            "startAt": 0,
+            "expand": "string"
+        }
+        try:
+            response2 = requests.get(
+                GET_ISSUES,
+                headers=headers,
+                params=params,
+                auth=auth
+            )
+        except requests.exceptions.RequestException as e:
+            st.error(f"Error connecting to Jira: {e}")
+            st.error(f"Response: {response2.text if 'response' in locals() else 'No response'}")
+        issue_result = json.loads(response2.text)
 
 
-normalized_df = pd.json_normalize(issue_result['issues'])
-base_url = "https://phdata.atlassian.net/browse/"
-normalized_df['link'] = base_url + normalized_df['key'] + '#' + normalized_df['key']
-columns_to_show = ['link', 'fields.summary']
+        normalized_df = pd.json_normalize(issue_result['issues'])
+        base_url = "https://phdata.atlassian.net/browse/"
+        normalized_df['link'] = base_url + normalized_df['key'] + '#' + normalized_df['key']
+        columns_to_show = ['link', 'fields.summary']
 
-filtered_df = normalized_df[columns_to_show]
-issue_count = len(filtered_df)
-st.text(f"You currently are assigned to or are watching {issue_count} issues.")
-st.dataframe(filtered_df,hide_index=True,
-    column_config={
-    "link": st.column_config.LinkColumn(
-        "Issue Key",
-        display_text=r".*#(.+)$"
-    ),
-    "fields.summary": "Summary",
-}
-)
+        filtered_df = normalized_df[columns_to_show]
+        issue_count = len(filtered_df)
 
-      
+
+        unique_projects = normalized_df['fields.project.key'].unique()
+        unique_project_count = len(unique_projects)
+
+        st.text(f"Below you will find the Jira issues that you are either assigned to or are watching, grouped by project.\n\nYou currrently have {issue_count} Jira issues in {unique_project_count} projects. You may bill time against each of these issues on the main 'Log time to Jira' page.")
+
+
+
+
+        for proj in unique_projects:
+            subset_df = filtered_df[normalized_df['fields.project.key'] == proj]
+            with st.container(border=True):
+                proj_link = "https://phdata.atlassian.net/jira/software/c/projects/"+proj+"/summary"
+                st.markdown(f"[{proj} Jira Summary]({proj_link})")
+                st.dataframe(subset_df,hide_index=True,
+                             column_config={
+                                 "link": st.column_config.LinkColumn(
+                                     "Issue Key",
+                                     display_text=r".*#(.+)$"
+                                 ),
+                                 "fields.summary": "Summary",
+                             })
+    else:
+        st.warning("Please add an API token to generate a summary of Jira issues.")
+except Exception as e:
+    st.error(f"Error: {e}")
 
 with st.expander("Components to build:"):
   st.markdown(":white_check_mark:   Input user email")
@@ -176,7 +201,7 @@ with st.expander("Components to build:"):
   st.markdown(":white_check_mark:   Collect user Jira id (requires user email and API key)")
   st.markdown(":white_check_mark:   For issues: use (assigned or watching) and != Done")
   st.markdown(":white_check_mark:   Build a display of issues")
-  st.markdown(":pencil2:   Hyperlink issue key")  
+  st.markdown(":white_check_mark:   Hyperlink issue key")  
   st.markdown(":pencil2:   Input allocations")
   
 
