a8c0b9e88d190a4c724a77dcf3d506b1e776f6da
philperrin <35937414+philperrin@users.noreply.github.com>
2026-01-20 06:38:43 -0700

    Update log.py

--- log.py
+++ log.py
@@ -9,11 +9,10 @@ with st.expander("First time here?"):
   st.markdown("Basic 'Getting started' instructions.")
 
 with st.expander("Components to build:"):
-  st.markdown(":pencil2:   Quick summary - hrs in Jira for day and week")
-  st.markdown(":pencil2:   How to best populate time to task.... log line to populate to df w/ commit button?")
-  st.markdown(":pencil2:   Required: date, task, start datetime, duration (or end time)")
-  st.markdown(":pencil2:   Optional: comments")
+  st.markdown(":pencil2:   Quick summary - display time logged in Jira for the day/week.")
+  st.markdown(":pencil2:   Required: date, task (drop down), start datetime, duration (or end time);Optional: comments")
   st.markdown(":pencil2:   Commit button")
+  st.markdown(":pencil2:   Button to pull in calendar events (user input for time period) - create dataframe with drop down for each item to log event to Jira")
 
 with st.expander("Functionalities:"):
   st.markdown(":firecracker:   Time log input device")
@@ -21,6 +20,8 @@ with st.expander("Functionalities:"):
   st.markdown("&nbsp;&nbsp;:boom:   Show input elements: date (cal input), task (dropdown from allocations), start (time), end (time), capture button")
   st.markdown("&nbsp;&nbsp;:boom:   Capture dataframe: each time capture button is clicked, add log details to dataframe")
   st.markdown("&nbsp;&nbsp;:boom:   Submit dataframe: send each log details to Jira, store log details to table (with extra details)")
+  st.markdown(":firecracker:   Pull events from Google Calendar")
+  st.markdown("&nbsp;&nbsp;:boom:   Only show if config details present")
   st.markdown(":firecracker:   Create new Jira")
   st.markdown("&nbsp;&nbsp;:boom:   Modal to create new Jira (drop down from current projects, assigns to self, etc)")
   st.markdown("&nbsp;&nbsp;:boom:   Then runs the script to pull in jiras based on filter.")
