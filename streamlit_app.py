1ab51aa58e7d6b794c45b04219dc8b5a9d328579
philperrin <35937414+philperrin@users.noreply.github.com>
2026-01-20 06:31:31 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,13 @@
 import streamlit as st
-
 st.set_page_config(
-    layout="centered"
-)
-
+    layout="centered")
 pages = {
     "Menu": [
         st.Page("log.py", title="Log time to Jira"),
         st.Page("config.py", title="Configure"),
+        st.Page("timeboxer.py", title="Timeboxer"),
         st.Page("reports.py", title="Reports"),
     ],
 }
-
 pg = st.navigation(pages, position="top")
 pg.run()
